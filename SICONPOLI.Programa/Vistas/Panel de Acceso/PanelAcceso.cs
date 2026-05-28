4d99e9ba2b97929f8937514a6068b0094fe4df67
Leo-Lunagit <rodriguezlunaleonardo534@gmail.com>
2026-05-28 07:27:31 -0600

    2nd. Nuevas utilidades del panel de acceso.
    Se agrego la logica para poder cambiar entre secciones de acceso y comodidades para el usuario.
    
    Se modifico la logica de la vista "PanelAcceso" para poder intercambiar y mostrar campos de inicio de secion a registro y biceversa.
    Se agregaron los iconos de ver o no ver contraseña.
    
    En la rama ActualizaconAcceso. | 2do commit.
    Cambios agregados:
        nuevo archivo: Recursos/Iconos/MostrarContra.png
        nuevo archivo: Recursos/Iconos/NoMostrarContra.png
        modificado: Vistas/Panel de acceso/PanelAcceso.cs
        modificado: Vistas/Panel de acceso/PanelAcceso.resx
        modificado: Vistas/Panel de acceso/PanelAcceso.Designer.cs
        modificado: Properties/Respources.resx
        modificado: Properties/Respources.Designer.cs
        modificado: PA4IM9-20262-Equipo2.csproj

--- SICONPOLI.Programa/Vistas/Panel de Acceso/PanelAcceso.cs
+++ SICONPOLI.Programa/Vistas/Panel de Acceso/PanelAcceso.cs
@@ -44,17 +44,17 @@ namespace PA4IM9_20262_Equipo2
         //
         // Logica de logueo.
         //
-        private void btnIniciarS_Click(object sender, EventArgs e)
+        private void btnAcceder_Click(object sender, EventArgs e)
         {
             // Verifica que SOLO los campos neseasarios No esten vacíos.
             if (txtUsuario.Text == "" || txtContrasenia.Text == "" || (txtNombre.Text == "" && txtNombre.Visible == true))
             {
                 // Si estan vacíos notifica con un texto y termina el proseso.
-                //lblMensajes.Text = "Campos vacios.";
+                lblMensajes.Text = "Campos vacios.";
                 return;
             }
             // EN CASO DE QUE NO ESTEN VACÍOS.
-            //lblMensajes.Text = ""; // Limpia el mensaje.
+            lblMensajes.Text = ""; // Limpia el mensaje.
 
             Sistema.VerificarArchivo(Sistema.rutaUsuarios, "usuarios"); // Verifica la existencia del archivo
             // Crea el controlador de archivos xml y carga el documento en la ruta de usuarios.
@@ -96,7 +96,7 @@ namespace PA4IM9_20262_Equipo2
                     }
                 }
                 // Si ningun registro coincide con los datos ingresados, lo notifica con un mensaje y borra la contraseña.
-                // lblMensajes.Text = "La información no coincide.";
+                lblMensajes.Text = "La información no coincide.";
                 txtContrasenia.Text = "";
             }
             // En caso de no existir ningun registro, se invita a registrarse amablemente.
@@ -106,7 +106,7 @@ namespace PA4IM9_20262_Equipo2
                 txtUsuario.Text = "";
                 txtContrasenia.Text = "";
                 // Mandar a llamar el registro.
-                AlternarInicioRegistro(50, false);
+                AlternarInicioRegistro(false);
             }
         }
         private void Registrarse(XmlDocument escritor, string nombre, string usuario, string contrasenia)
@@ -135,6 +135,50 @@ namespace PA4IM9_20262_Equipo2
             // Accede al sistema.
             MessageBox.Show("Datos correctos. | Accediste al sistema.", "Acceso Permitido.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
         }
+
+        //
+        // Logica de apariencias.
+        //
+        private void AlternarInicioRegistro(bool deInicioToRegistro)
+        {
+            // Ajustar tamaños.
+            // Si cambia a registro se deben modificar las propiedades (el mismo numero {1}),
+            // si cambia a inicio de secion se deben contrarestar las modificaiones (el inverso del numero {-1}).
+            int sentido = deInicioToRegistro ? 1 : -1;  
+            grpInicioS.Height += 110 * sentido; // 130px entre 4 text y espacios, pero 20px menos del boton de recuperar contraseña
+            // Desplazar los controles un sierto tamaño.
+            grpInicioS.Location = new Point(grpInicioS.Location.X, grpInicioS.Location.Y - 50 * sentido ); // Hacia arriba (signo -) hay 50px a donde nos podemos mover.
+            chkBoxRecordar.Location = new Point(chkBoxRecordar.Location.X, chkBoxRecordar.Location.Y + 20 * sentido); // Tomamos el espacio por debajo (signo +)
+            btnAcceder.Location = new Point(btnAcceder.Location.X, btnAcceder.Location.Y + 20 * sentido); // correspondiente al boton de recuperar contraseña.
+            txtUsuario.Location = new Point(txtUsuario.Location.X, txtUsuario.Location.Y + 35 * sentido); // 25px del alto de los txt y 12 px del espacio entre ellos.
+            txtCorreo.Location = new Point(txtCorreo.Location.X, txtCorreo.Location.Y + 35 * 2 * sentido); // Esta en la primera posicion y debe llegar a la 3, con 2 txt por debajo.
+            txtContrasenia.Location = new Point(txtContrasenia.Location.X, txtContrasenia.Location.Y + 35 * 2 * sentido); // Esta en la segunda posicion y debe llegar a la cuarta.
+            iconoContrasenia.Location = new Point(iconoContrasenia.Location.X, iconoContrasenia.Location.Y + 35 * 2 * sentido);
+            txtConfirmarContra.Location = new Point(txtConfirmarContra.Location.X, txtConfirmarContra.Location.Y + 35 * 3 * sentido);
+            iconoConfirmarContra.Location = new Point(iconoConfirmarContra.Location.X, iconoConfirmarContra.Location.Y + 35 * 3 * sentido);
+            txtEdad.Location = new Point(txtEdad.Location.X, txtEdad.Location.Y + 35 * 4 * sentido);
+            // Mostrar controles extra.
+            btnRecuperar.Visible = !deInicioToRegistro;
+            txtNombre.Visible = deInicioToRegistro;
+            txtCorreo.Visible = deInicioToRegistro;
+            txtConfirmarContra.Visible = deInicioToRegistro;
+            iconoConfirmarContra.Visible = deInicioToRegistro;
+            txtEdad.Visible = deInicioToRegistro;
+            // Cambiar textos.
+            lblMensajes.Text = "";
+            grpInicioS.Text = deInicioToRegistro ? "Registrarse en SIRETECH Poli" : "Iniciar Sesión en SIRETECH Poli";
+            btnAcceder.Text = deInicioToRegistro ? "Registrarse" : "Iniciar Sesión";
+            btnAlternar.Text = deInicioToRegistro ? "Iniciar Sesión" : "Registrarse";
+        }
+        private void btnAlternar_Click(object sender, EventArgs e)
+        {
+            // Transformamos al objeto que envia los datos en un control.
+            Control boton = sender as Control;
+            // Si el texto del control coincide, alterna a registro.
+            if (boton.Text == "Registrarse") AlternarInicioRegistro(true);
+            // Si no, alterna al inicio de secion.
+            else AlternarInicioRegistro(false);
+        }
         //
         // Logica de ventanas.
         //
@@ -145,34 +189,76 @@ namespace PA4IM9_20262_Equipo2
         // Cargar ventana de "Recuperar contraseña" Solo minimizar la de registro.
         private void btnRecuperar_Click(object sender, EventArgs e) { MessageBox.Show("Enviaremos un correo a su direccion", "Recuperar.", MessageBoxButtons.OK, MessageBoxIcon.Information); }
         //
-        // Logica de apariencias.
+        // Logica de utiilidades.
         //
-        private void AlternarInicioRegistro(int desplazamiento, bool inicio)
+        private void QuitarPlaceHolder(object Sender, string PlaceHolder)
         {
-            // Ajustar tamaños.
-            int ajuste = !inicio ? -20 : 20; // 20px del boton para recuperar la contraseña.
-            this.MaximumSize = new Size(this.Width, this.Height + desplazamiento + ajuste);
-            this.MinimumSize = new Size(this.Width, this.Height + desplazamiento + ajuste);
-            grpInicioS.Height += desplazamiento + ajuste;
-            // Desplazar los controles un sierto tamaño.
-            txtUsuario.Location = new Point(txtUsuario.Location.X, txtUsuario.Location.Y + desplazamiento);
-            txtContrasenia.Location = new Point(txtContrasenia.Location.X, txtContrasenia.Location.Y + desplazamiento);
-            chkBoxRecordar.Location = new Point(chkBoxRecordar.Location.X, chkBoxRecordar.Location.Y + desplazamiento);
-            btnAcceder.Location = new Point(btnAcceder.Location.X, btnAcceder.Location.Y + desplazamiento);
-            btnRecuperar.Location = new Point(btnRecuperar.Location.X, btnRecuperar.Location.Y + desplazamiento);
-            // Mostrar controles extra.
-            btnRecuperar.Visible = inicio;
-            txtNombre.Visible = !inicio;
-            // Cambiar textos.
-            // lblMensajes.Text = "";
-            grpInicioS.Text = inicio ? "Iniciar Sesión en SICONPOLI" : "Registrarse en SICONPOLI";
-            btnAcceder.Text = inicio ? "Iniciar Sesión" : "Registrarse";
-            btnRegistrar.Text = inicio ? "Registrarse" : "Iniciar Sesión";
+            // Transforma el objeto enviado en un control.
+            Control caja = Sender as Control;
+            if (caja.Text == PlaceHolder)
+            {
+                // Si el texto es igual al "placeholder" lo borra. 
+                caja.Text = "";
+                // Y le da un color nitido.
+                caja.ForeColor = Color.FromArgb(0, 0, 0);
+            }
         }
-
-        private void btnRegistrar_Click(object sender, EventArgs e)
+        private void PonerPlaceHolder(object Sender, string PlaceHolder)
         {
-
+            Control caja = Sender as Control;
+            if (caja.Text == "")
+            {
+                // Si el texto esta vacio da el texto del placeholder.
+                caja.Text = PlaceHolder;
+                // Y regresa el color claro.
+                caja.ForeColor = Color.FromArgb(99, 101, 105);
+            }
         }
+        private void QuitarPlaceHolderContrasenias(object Sender, string PlaceHolder)
+        {
+            TextBox caja = Sender as TextBox;
+            if (caja.Text == PlaceHolder)
+            {
+                caja.PasswordChar = '*'; // Hace que aparescan asteriscos en ves de la contraseña.
+                caja.Text = "";
+                caja.ForeColor = Color.FromArgb(0, 0, 0);
+            }
+        }
+        private void PonerPlaceHolderContrasenias(object Sender, string PlaceHolder)
+        {
+            TextBox caja = Sender as TextBox;
+            if (caja.Text == "")
+            {
+                caja.PasswordChar = '\0'; // Hace que vuelvan a aparecer letras
+                caja.Text = PlaceHolder;
+                caja.ForeColor = Color.FromArgb(99, 101, 105);
+            }
+        }
+        private void txtNombre_Enter(object sender, EventArgs e) { QuitarPlaceHolder(sender, "Nombre"); }
+        private void txtNombre_Leave(object sender, EventArgs e) { PonerPlaceHolder(sender, "Nombre"); }
+        private void txtUsuario_Enter(object sender, EventArgs e) { QuitarPlaceHolder(sender, "Usuario"); }
+        private void txtUsuario_Leave(object sender, EventArgs e) { PonerPlaceHolder(sender, "Usuario"); }
+        private void txtCorreo_Enter(object sender, EventArgs e) { QuitarPlaceHolder(sender, "Email"); } 
+        private void txtCorreo_Leave(object sender, EventArgs e) { PonerPlaceHolder(sender, "Email"); }
+        private void txtContrasenia_Enter(object sender, EventArgs e) { QuitarPlaceHolderContrasenias(sender, "Contraseña"); }
+        private void txtContrasenia_Leave(object sender, EventArgs e) { PonerPlaceHolderContrasenias(sender, "Contraseña"); }
+        private void txtConfirmarContra_Enter(object sender, EventArgs e) { QuitarPlaceHolderContrasenias(sender, "Confirmar Contraseña"); }
+        private void txtConfirmarContra_Leave(object sender, EventArgs e) { PonerPlaceHolderContrasenias(sender, "Confirmar Contraseña"); }
+        private void txtEdad_Enter(object sender, EventArgs e) { QuitarPlaceHolder(sender, "Edad"); }
+        private void txtEdad_Leave(object sender, EventArgs e) { PonerPlaceHolder(sender, "Edad"); }
+
+        private void AlternarVerContra(object Sender, object MarcoIcono, string PlaceHolder)
+        {
+            TextBox caja = Sender as TextBox;
+            if (caja.Text == PlaceHolder) return;
+            PictureBox icono = MarcoIcono as PictureBox;
+            // Con una verificacion intercambia al caracter de contraseña correcto.
+            bool Mostrar = caja.PasswordChar == '\0';
+            caja.PasswordChar = Mostrar ? '*' : '\0';
+            // Cambia de icono al correspondiente.
+            icono.Image = Mostrar ? Properties.Resources.NoMostrarContra : Properties.Resources.MostrarContra;
+        }
+        private void iconoContrasenia_Click(object sender, EventArgs e) { AlternarVerContra(txtContrasenia, sender, "Contraseña"); }
+        private void iconoConfirmarContra_Click(object sender, EventArgs e) { AlternarVerContra(txtConfirmarContra, sender, "Confirmar Contraseña"); }
     }
 }
