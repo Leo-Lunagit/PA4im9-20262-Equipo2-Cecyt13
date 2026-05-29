7ef386c88b46e628b6de5c93a354794ca734a8f2
Leo-Lunagit <rodriguezlunaleonardo534@gmail.com>
2026-05-28 20:41:34 -0600

    Integrando panel de compras

---
+++ SICONPOLI.Programa/Vistas/PanelCompras/Panel de compras.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace PA4IM9_20262_Equipo2
+{
+    partial class Panel_de_compras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProductos = new System.Windows.Forms.Label();
+            this.lblIVA = new System.Windows.Forms.Label();
+            this.lblPreciofinal = new System.Windows.Forms.Label();
+            this.lblProveedores = new System.Windows.Forms.Label();
+            this.lblCostounitario = new System.Windows.Forms.Label();
+            this.lblTotaldeproductos = new System.Windows.Forms.Label();
+            this.txtTotalProductos = new System.Windows.Forms.TextBox();
+            this.cmbProductos = new System.Windows.Forms.ComboBox();
+            this.nudCantidad = new System.Windows.Forms.NumericUpDown();
+            this.txtIVA = new System.Windows.Forms.TextBox();
+            this.txtPrecioFinal = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtProveedor = new System.Windows.Forms.TextBox();
+            this.nudCostoUnitario = new System.Windows.Forms.NumericUpDown();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.dgvSubcuentas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCostoUnitario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSubcuentas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProductos
+            // 
+            this.lblProductos.AutoSize = true;
+            this.lblProductos.Location = new System.Drawing.Point(173, 40);
+            this.lblProductos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblProductos.Name = "lblProductos";
+            this.lblProductos.Size = new System.Drawing.Size(87, 21);
+            this.lblProductos.TabIndex = 0;
+            this.lblProductos.Text = "Productos";
+            // 
+            // lblIVA
+            // 
+            this.lblIVA.AutoSize = true;
+            this.lblIVA.Location = new System.Drawing.Point(204, 226);
+            this.lblIVA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIVA.Name = "lblIVA";
+            this.lblIVA.Size = new System.Drawing.Size(37, 21);
+            this.lblIVA.TabIndex = 1;
+            this.lblIVA.Text = "IVA";
+            // 
+            // lblPreciofinal
+            // 
+            this.lblPreciofinal.AutoSize = true;
+            this.lblPreciofinal.Location = new System.Drawing.Point(173, 293);
+            this.lblPreciofinal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPreciofinal.Name = "lblPreciofinal";
+            this.lblPreciofinal.Size = new System.Drawing.Size(99, 21);
+            this.lblPreciofinal.TabIndex = 4;
+            this.lblPreciofinal.Text = "Precio Final";
+            // 
+            // lblProveedores
+            // 
+            this.lblProveedores.AutoSize = true;
+            this.lblProveedores.Location = new System.Drawing.Point(571, 40);
+            this.lblProveedores.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblProveedores.Name = "lblProveedores";
+            this.lblProveedores.Size = new System.Drawing.Size(106, 21);
+            this.lblProveedores.TabIndex = 5;
+            this.lblProveedores.Text = "Proveedores";
+            // 
+            // lblCostounitario
+            // 
+            this.lblCostounitario.AutoSize = true;
+            this.lblCostounitario.Location = new System.Drawing.Point(413, 40);
+            this.lblCostounitario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCostounitario.Name = "lblCostounitario";
+            this.lblCostounitario.Size = new System.Drawing.Size(117, 21);
+            this.lblCostounitario.TabIndex = 6;
+            this.lblCostounitario.Text = "Costo Unitario";
+            // 
+            // lblTotaldeproductos
+            // 
+            this.lblTotaldeproductos.AutoSize = true;
+            this.lblTotaldeproductos.Location = new System.Drawing.Point(173, 163);
+            this.lblTotaldeproductos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotaldeproductos.Name = "lblTotaldeproductos";
+            this.lblTotaldeproductos.Size = new System.Drawing.Size(155, 21);
+            this.lblTotaldeproductos.TabIndex = 8;
+            this.lblTotaldeproductos.Text = "Total de productos";
+            // 
+            // txtTotalProductos
+            // 
+            this.txtTotalProductos.Enabled = false;
+            this.txtTotalProductos.Location = new System.Drawing.Point(356, 160);
+            this.txtTotalProductos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTotalProductos.Name = "txtTotalProductos";
+            this.txtTotalProductos.Size = new System.Drawing.Size(102, 27);
+            this.txtTotalProductos.TabIndex = 10;
+            this.txtTotalProductos.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // cmbProductos
+            // 
+            this.cmbProductos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProductos.FormattingEnabled = true;
+            this.cmbProductos.Location = new System.Drawing.Point(140, 82);
+            this.cmbProductos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbProductos.Name = "cmbProductos";
+            this.cmbProductos.Size = new System.Drawing.Size(157, 29);
+            this.cmbProductos.TabIndex = 12;
+            // 
+            // nudCantidad
+            // 
+            this.nudCantidad.Location = new System.Drawing.Point(325, 82);
+            this.nudCantidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nudCantidad.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nudCantidad.Name = "nudCantidad";
+            this.nudCantidad.Size = new System.Drawing.Size(48, 27);
+            this.nudCantidad.TabIndex = 13;
+            // 
+            // txtIVA
+            // 
+            this.txtIVA.Enabled = false;
+            this.txtIVA.Location = new System.Drawing.Point(356, 226);
+            this.txtIVA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtIVA.Name = "txtIVA";
+            this.txtIVA.Size = new System.Drawing.Size(102, 27);
+            this.txtIVA.TabIndex = 16;
+            // 
+            // txtPrecioFinal
+            // 
+            this.txtPrecioFinal.Enabled = false;
+            this.txtPrecioFinal.Location = new System.Drawing.Point(356, 287);
+            this.txtPrecioFinal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPrecioFinal.Name = "txtPrecioFinal";
+            this.txtPrecioFinal.Size = new System.Drawing.Size(102, 27);
+            this.txtPrecioFinal.TabIndex = 18;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(575, 220);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(112, 38);
+            this.btnRegistrar.TabIndex = 19;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtProveedor
+            // 
+            this.txtProveedor.Location = new System.Drawing.Point(575, 82);
+            this.txtProveedor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtProveedor.Name = "txtProveedor";
+            this.txtProveedor.Size = new System.Drawing.Size(148, 27);
+            this.txtProveedor.TabIndex = 20;
+            // 
+            // nudCostoUnitario
+            // 
+            this.nudCostoUnitario.Location = new System.Drawing.Point(433, 84);
+            this.nudCostoUnitario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nudCostoUnitario.Maximum = new decimal(new int[] {
+            1410065408,
+            2,
+            0,
+            0});
+            this.nudCostoUnitario.Name = "nudCostoUnitario";
+            this.nudCostoUnitario.Size = new System.Drawing.Size(80, 27);
+            this.nudCostoUnitario.TabIndex = 21;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(307, 40);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(85, 21);
+            this.lblCantidad.TabIndex = 23;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // dgvSubcuentas
+            // 
+            this.dgvSubcuentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSubcuentas.Location = new System.Drawing.Point(98, 360);
+            this.dgvSubcuentas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvSubcuentas.Name = "dgvSubcuentas";
+            this.dgvSubcuentas.Size = new System.Drawing.Size(711, 336);
+            this.dgvSubcuentas.TabIndex = 24;
+            // 
+            // Panel_de_compras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(908, 706);
+            this.Controls.Add(this.dgvSubcuentas);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.nudCostoUnitario);
+            this.Controls.Add(this.txtProveedor);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtPrecioFinal);
+            this.Controls.Add(this.txtIVA);
+            this.Controls.Add(this.nudCantidad);
+            this.Controls.Add(this.cmbProductos);
+            this.Controls.Add(this.txtTotalProductos);
+            this.Controls.Add(this.lblTotaldeproductos);
+            this.Controls.Add(this.lblCostounitario);
+            this.Controls.Add(this.lblProveedores);
+            this.Controls.Add(this.lblPreciofinal);
+            this.Controls.Add(this.lblIVA);
+            this.Controls.Add(this.lblProductos);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DarkRed;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Panel_de_compras";
+            this.Text = "Panel_de_compras";
+            this.Load += new System.EventHandler(this.Panel_de_compras_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCostoUnitario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSubcuentas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProductos;
+        private System.Windows.Forms.Label lblIVA;
+        private System.Windows.Forms.Label lblPreciofinal;
+        private System.Windows.Forms.Label lblProveedores;
+        private System.Windows.Forms.Label lblCostounitario;
+        private System.Windows.Forms.Label lblTotaldeproductos;
+        private System.Windows.Forms.TextBox txtTotalProductos;
+        private System.Windows.Forms.ComboBox cmbProductos;
+        private System.Windows.Forms.NumericUpDown nudCantidad;
+        private System.Windows.Forms.TextBox txtIVA;
+        private System.Windows.Forms.TextBox txtPrecioFinal;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtProveedor;
+        private System.Windows.Forms.NumericUpDown nudCostoUnitario;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.DataGridView dgvSubcuentas;
+    }
+}
---
+++ SICONPOLI.Programa/Vistas/PanelCompras/Panel de compras.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA4IM9_20262_Equipo2
+{
+    public partial class Panel_de_compras : Form
+    {
+        public Panel_de_compras()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel_de_compras_Load(object sender, EventArgs e)
+        {
+            // 1. Agregar productos de ejemplo al ComboBox
+            cmbProductos.Items.Add("Laptop Dell");
+            cmbProductos.Items.Add("Monitor 24''");
+            cmbProductos.Items.Add("Teclado Mecánico");
+            cmbProductos.Items.Add("Mouse Ergonómico");
+            cmbProductos.SelectedIndex = 0; // Selecciona el primero por defecto
+
+            // 2. Configurar las propiedades de los controles numéricos
+            nudCantidad.Minimum = 1;
+            nudCostoUnitario.DecimalPlaces = 2;
+            nudCostoUnitario.Maximum = 100000;
+
+            // 3. Configurar las columnas del DataGridView (el apartado de subcuentas)
+            dgvSubcuentas.Columns.Add("Producto", "Producto");
+            dgvSubcuentas.Columns.Add("Cantidad", "Cantidad");
+            dgvSubcuentas.Columns.Add("CostoUnitario", "Costo Unitario");
+            dgvSubcuentas.Columns.Add("Total", "Total Producto");
+            dgvSubcuentas.Columns.Add("Proveedor", "Proveedor");
+
+            // Ajuste estético para que las columnas ocupen todo el ancho disponible
+            dgvSubcuentas.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+        
+            // 1. Validar que el campo Proveedor no esté vacío
+            if (string.IsNullOrWhiteSpace(txtProveedor.Text))
+            {
+                MessageBox.Show("Por favor, ingresa el nombre del proveedor.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 2. Capturar los datos de los controles
+            string producto = cmbProductos.SelectedItem.ToString();
+            int cantidad = (int)nudCantidad.Value;
+            decimal costoUnitario = nudCostoUnitario.Value;
+            string proveedor = txtProveedor.Text.Trim();
+
+            // 3. Calcular el total de esta subcuenta específica
+            decimal totalProducto = cantidad * costoUnitario;
+
+            // 4. Agregar la fila al DataGridView
+            dgvSubcuentas.Rows.Add(producto, cantidad, costoUnitario.ToString("C2"), totalProducto.ToString("C2"), proveedor);
+
+            // 5. Recalcular los totales generales de la aplicación
+            CalcularTotalesGenerales();
+
+            // 6. Limpiar los campos para la siguiente captura (Libertad creativa de UX)
+            LimpiarCamposCaptura();
+        }
+        private void CalcularTotalesGenerales()
+        {
+            decimal subtotal = 0;
+            const decimal PorcentajeIVA = 0.16m; // Ajustable según la tasa de tu región (ej. 16%)
+
+            // Recorrer cada fila del DataGridView para sumar los totales de los productos
+            foreach (DataGridViewRow fila in dgvSubcuentas.Rows)
+            {
+                if (fila.Cells["Total"].Value != null)
+                {
+                    // Reemplazamos el símbolo de moneda para poder convertirlo a decimal limpiamente
+                    string valorTexto = fila.Cells["Total"].Value.ToString().Replace("$", "");
+                    subtotal += Convert.ToDecimal (valorTexto);
+                }
+            }
+
+            // Calcular IVA y Precio Final
+            decimal iva = subtotal * PorcentajeIVA;
+            decimal precioFinal = subtotal + iva;
+
+            // Mostrar los resultados formateados como moneda local
+            txtTotalProductos.Text = subtotal.ToString("C2");
+            txtIVA.Text = iva.ToString("C2");
+            txtPrecioFinal.Text = precioFinal.ToString("C2");
+        }
+
+        private void LimpiarCamposCaptura()
+        {
+            // Restablece los controles superiores para agilizar el rellenado
+            if (cmbProductos.Items.Count > 0) cmbProductos.SelectedIndex = 0;
+            nudCantidad.Value = 1;
+            nudCostoUnitario.Value = 0;
+            txtProveedor.Clear();
+            cmbProductos.Focus(); // Pone el cursor de vuelta en el producto
+        }
+    }
+    }
