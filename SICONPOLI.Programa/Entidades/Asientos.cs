2d33eec3d352dd9ae81b76a3d1b6da81f144b496
Lunajmr-git <rodriguezlunaleonardo13@gmail.com>
2026-05-29 10:07:40 -0600

    1.0.1  Juntando las entidades para asientos.
    Se indexo un conjunto de entidadades hija de subcuenta en la entidad padre cuenta y a su vez un conjunto de estas en la entidad padre (asiento).
    Se vinculo la estructura de arbol para su facil conversion a elementos Xml.
    
    En la rama Aphl1.0.0 | Segundo cambio. Por Luna
    Cambios agregados:
        modificado: Entidades/Asiento.cs

--- SICONPOLI.Programa/Entidades/Asientos.cs
+++ SICONPOLI.Programa/Entidades/Asientos.cs
@@ -8,34 +8,43 @@ using System.Xml.Serialization;
 namespace PA4IM9_20262_Equipo2.Entidades
 {
     //Dudas checar el apartado de "Perfil"
-    [XmlRoot("subcuenta")]
     public class Subcuenta
     {
         [XmlElement("nombreSubcuenta")]
         public string NombreSubcuenta { get; set; }
         [XmlElement("monto")]
-        public double Monto { get; set; }
+        public int Monto { get; set; }
     }
-    [XmlRoot("cuenta")]
     public class Cuenta
     {
         [XmlElement("nombreCuenta")]
         public string NombreCuenta { get; set; }
-        [XmlElement("subCuentas")]
-        public string[] Subcuentas { get; set; }
+        [XmlArray("subcuentas")] // Indicamos un elemento contenedor.
+        [XmlArrayItem("subcuenta")] // Indicamos el elemento por cada contenido del array.
+        public Subcuenta[] Subcuentas { get; set; }
+        [XmlElement("monto")]
+        public int Monto { get; set; }
     }
-
-    [XmlRoot("asiento")]
+    // Unicamente este tendra la etiqueta root, solo este sera el elemento padre, no puede aver mas de uno.
+    // Para cada clase se le especifica la etiqueta contenedora (su padre) con XmlElement o XmlArray.
+    [XmlRoot("asiento")] 
     public class Asiento
     {
+        [XmlAttribute("noAsiento")] // Indica que no sera un atributo, si no un metadato.
+        public int NoAsiento { get; set; }
         [XmlElement("fecha")]
         public DateTime Fecha { get; set; }
+        [XmlArray("cargos")]
+        [XmlArrayItem("cuenta")]
+        public Cuenta[] Cargos { get; set; }
+        [XmlArray("abonos")]
+        [XmlArrayItem("cuenta")]
+        public Cuenta[] Abonos { get; set; }
         [XmlElement("sumaCargos")]
-        public double SumaCargos { get; set; }
+        public int SumaCargos { get; set; }
         [XmlElement("sumaAbonos")]
-        public double SumaAbonos { get; set; }
+        public int SumaAbonos { get; set; }
         [XmlElement("concepto")]
         public string Concepto { get; set; }
     }
-
 }
